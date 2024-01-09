475dce8ce3b91ab2c4fa5bf995ee33514643996c
User <User@BERTHA>
2024-01-09 09:34:11 -0500

    Add project files.

---
+++ Snake/Audio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using System.Windows.Media;
+
+namespace snake
+{
+
+    public static class Audio
+    {
+        public readonly static MediaPlayer GameOver = LoadAudio("goofy-ahh-android-low-quality.wav");
+        private static MediaPlayer LoadAudio(string filename, bool autoReset = true)
+        {
+            MediaPlayer player = new();
+            player.Open(new Uri($"Assets/{filename}", UriKind.Relative));
+            player.Volume = 10;
+            return player;
+        }
+
+
+
+
+
+    }
+
+
+}
